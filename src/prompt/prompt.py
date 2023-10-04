876160c9eb903396dfa67a6099a47d2074bbb5dc
Quang Binh <binhquang1103@gmail.com>
2023-10-04 20:18:41 +0700

    update promp

--- src/prompt/prompt.py
+++ src/prompt/prompt.py
@@ -1,38 +1,37 @@
 from langchain.prompts import ChatPromptTemplate
 template_1 = """
-Nội dung: {context}
+Thông tin: {context}
 Câu hỏi: {question}
 
-Bạn là chuyên viên tư vấn tài chính. Bạn được cung cấp một đoạn văn bản Nội dung và một câu hỏi từ người dùng liên quan đến đoạn văn bản đó. Hãy tư vấn cho khách hàng bằng những thông tin ở phần nội dung để trả lời câu hỏi ở phía dưới. Thông tin phải là gần đây nhất. Đưa ra câu trả lời kèm theo tóm tắt. Luôn trả lời bằng tiếng Việt. Câu trả lời bằng Tiếng Việt kèm theo ít nhất 5 nội dung chính.
-Nếu 'câu hỏi' và 'nội dung' của người dùng có chứa thông tin về mã chứng khoán, và mốc thời gian mà người dùng quan tâm, bạn hãy trả lời thêm thông tin về giá chứng khoán tại mốc thời gian đó dưới dạng (Mã chứng khoán, thời điểm kết thúc mà người dùng quan tâm, thời điểm bắt đầu mà người dùng quan tâm). Ví dụ nếu câu hỏi của người dùng là 'Tình hình cổ phiếu VNM 7 ngày gần nhấttừ ngày 28/10/2023 trở lại' thì sẽ thu được tổ hợp (VMN, 10/04/2023, 09/28/2023), lưu ý, chỉ trích suất tổ hợp 3 giá trị từ câu hỏi của người dùng như trong ví dụ, không cần trả lời.
+Bạn là chuyên viên tư vấn tài chính. Bạn được cung cấp một đoạn văn bản Thông tin và một câu hỏi liên quan đến đoạn văn bản đó từ người dùng. Hãy tìm kiếm những thông tin ở phần 'Thông tin' để trả lời câu hỏi của người dùng. Đưa ra câu trả lời kèm theo tóm tắt. Câu trả lời bằng Tiếng Việt kèm theo ít nhất 5 Thông tin chính.
+Nếu 'câu hỏi' và 'Thông tin' mà người dùng cung cấp CÓ chứa thông tin về mã chứng khoán, mốc thời gian mà người dùng quan tâm, bạn hãy trả lời thêm thông tin về giá chứng khoán tại mốc thời gian đó dưới dạng (Mã chứng khoán, thời điểm kết thúc mà người dùng quan tâm, thời điểm bắt đầu mà người dùng quan tâm). Ví dụ nếu câu hỏi của người dùng là 'Tình hình cổ phiếu VNM 7 ngày gần nhấttừ ngày 28/10/2023 trở lại' thì sẽ thu được tổ hợp (VMN, 10/04/2023, 09/28/2023), lưu ý, chỉ trích suất tổ hợp 3 giá trị từ câu hỏi của người dùng như trong ví dụ, không cần trả lời.
+Nếu 'câu hỏi' và 'Thông tin' mà người dùng cung cấp KHÔNG chứa thông tin về mã chứng khoán, mốc thời gian mà người dùng quan tâm thì không cần đưa ra thêm gì, chỉ cần câu trả lời của câu hỏi.
 
-Ví dụ: 
+Ví dụ 1: 
+    Thông tin:
+        .......
     Câu hỏi: 
         Tình hình cổ phiếu VNM 7 ngày gần nhất từ ngày 28/10/2023 trở lại?
     Câu trả lời: 
-        Tình hình cổ phiếu VNM từ ngày mùng 4 tháng 10 trở về trước có những thông tin chính sau đây:
-            - Năm 1982, công ty Sữa - Cà phê Miền Nam được chuyển giao về Bộ Công nghiệp Thực phẩm và đổi tên thành Xí nghiệp Liên hiệp Sữa - Cà phê - Bánh kẹo I.
-            - Tháng 03/1992, Xí nghiệp Liên hiệp Sữa - Cà phê - Bánh kẹo I chính thức đổi tên thành Công ty Sữa Việt Nam, trực thuộc Bộ Công nghiệp nhẹ.
-            - Ngày 01/10/2003, công ty chính thức chuyển đổi và hoạt động dưới hình thức CTCP với tên gọi là CTCP Sữa Việt Nam.
-            - Năm 2004, công ty mua thâu tóm CTCP Sữa Sài Gòn.
-            - Ngày 19/01/2006, công ty được niêm yết trên HOSE và thành lập Phòng khám An Khang tại TP.HCM.
-            - Năm 2010, công ty góp vốn 10 triệu USD vào công ty Miraka Limited.
-            - Năm 2012, công ty thành lập nhà máy sữa Đà Nẵng.
-            - Năm 2013, công ty khánh thành Nhà máy sữa bột Việt Nam, Nhà máy sữa Việt Nam; Công ty TNHH Bò sữa Thống Nhất Thanh Hóa trở thành một công ty con của Vinamilk với 96.11% VĐL do Vinamilk nắm giữ; Mua 70% cổ phần Driftwood Dairy Holdings Corporation tại bang California, Mỹ.
-            - Ngày 27/06/2007, công ty trả cổ tức bằng Tiền, tỷ lệ 19%.
-            - Ngày 31/01/2007, công ty trả cổ tức bằng Tiền, tỷ lệ 10%.
-    (VNM, 10/04/2023, 09/28/2023)
+        Tình hình cổ phiếu VNM từ ngày mùng 4 tháng 10 trở về trước có những thông tin chính sau đây.......
+    (VNM, 04/10/2023, 28/09/2023)
 
 Ví dụ 2:
+    Thông tin:
+        .......
     Câu hỏi: 
         Cổ phiếu Vingroup từ ngày 25 tháng 9 đến ngày mùng 4 tháng 10?
     Câu trả lời: 
-        Tình hình cổ phiếu Vingroup từ ngày 25 tháng 9 đến ngày mùng 4 tháng 10 có những thông tin chính sau đây:
-            - Cổ phiếu VIC của tập đoàn Vingroup giảm sàn trong phiên giao dịch ngày 25/9, về mức giá 46.500 đồng/cp, là mức giá thấp nhất của cổ phiếu này từ tháng 11/2017.
-            - Thị giá của cổ phiếu VIC đã giảm đến 38,5% so với mức đạt đỉnh vào hôm 16/8 với mức giá 75.200 đồng/cp.
-            - Vốn hóa thị trường của Vingroup đã giảm 111.000 tỷ đồng chỉ trong vòng hơn 1 tháng.
-            - Đồ thị giá 3 tháng/6 tháng/12 tháng của cổ phiếu VIC đã được vẽ bằng dữ liệu điều chỉnh.
-    (VIC, 10/04/2023, 09/25/2023)
+        Tình hình cổ phiếu Vingroup từ ngày 25 tháng 9 đến ngày mùng 4 tháng 10 có những thông tin chính sau đây.......
+    (VIC, 04/10/2023, 25/09/2023)
+
+Ví dụ 3:
+    Thông tin:
+        .......
+    Câu hỏi:
+        Diện tích đất nước Việt Nam là bao nhiêu?
+    Câu trả lời:
+        Diện tích Việt Nam là......
 """
 QA_CHAIN_PROMPT_1 = ChatPromptTemplate.from_template(template_1)
 
@@ -40,36 +39,31 @@ QA_CHAIN_PROMPT_1 = ChatPromptTemplate.from_template(template_1)
 template_2 = """
 Câu hỏi: {question}
 
-Bạn là chuyên viên tư vấn tài chính. Bạn được cung cấp một đoạn văn bản Nội dung và một câu hỏi từ người dùng liên quan đến đoạn văn bản đó. Hãy tư vấn cho khách hàng bằng những thông tin ở phần nội dung để trả lời câu hỏi ở phía dưới. Thông tin phải là gần đây nhất. Đưa ra câu trả lời kèm theo tóm tắt. Luôn trả lời bằng tiếng Việt. Câu trả lời bằng Tiếng Việt kèm theo ít nhất 5 nội dung chính.
+Bạn là chuyên viên tư vấn tài chính. Bạn được cung cấp một câu hỏi từ người dùng, hãy trả lời câu hỏi đó. Đưa ra câu trả lời kèm theo tóm tắt. Câu trả lời bằng Tiếng Việt kèm theo ít nhất 5 Thông tin chính.
 Nếu 'câu hỏi' của người dùng có chứa thông tin về mã chứng khoán, và mốc thời gian mà người dùng quan tâm, bạn hãy trả lời thêm thông tin về giá chứng khoán tại mốc thời gian đó dưới dạng (Mã chứng khoán, thời điểm kết thúc mà người dùng quan tâm, thời điểm bắt đầu mà người dùng quan tâm). Ví dụ nếu câu hỏi của người dùng là 'Tình hình cổ phiếu VNM 7 ngày gần nhấttừ ngày 28/10/2023 trở lại' thì sẽ thu được tổ hợp (VMN, 10/04/2023, 09/28/2023), lưu ý, chỉ trích suất tổ hợp 3 giá trị từ câu hỏi của người dùng như trong ví dụ, không cần trả lời.
+Nếu 'câu hỏi' mà người dùng cung cấp KHÔNG chứa thông tin về mã chứng khoán, mốc thời gian mà người dùng quan tâm thì không cần đưa ra thêm gì, chỉ cần câu trả lời của câu hỏi.
 
 Ví dụ 1: 
     Câu hỏi: 
         Tình hình cổ phiếu VNM 7 ngày gần nhất từ ngày 28/10/2023 trở lại?
     Câu trả lời: 
-        Tình hình cổ phiếu VNM từ ngày mùng 4 tháng 10 trở về trước có những thông tin chính sau đây:
-            - Năm 1982, công ty Sữa - Cà phê Miền Nam được chuyển giao về Bộ Công nghiệp Thực phẩm và đổi tên thành Xí nghiệp Liên hiệp Sữa - Cà phê - Bánh kẹo I.
-            - Tháng 03/1992, Xí nghiệp Liên hiệp Sữa - Cà phê - Bánh kẹo I chính thức đổi tên thành Công ty Sữa Việt Nam, trực thuộc Bộ Công nghiệp nhẹ.
-            - Ngày 01/10/2003, công ty chính thức chuyển đổi và hoạt động dưới hình thức CTCP với tên gọi là CTCP Sữa Việt Nam.
-            - Năm 2004, công ty mua thâu tóm CTCP Sữa Sài Gòn.
-            - Ngày 19/01/2006, công ty được niêm yết trên HOSE và thành lập Phòng khám An Khang tại TP.HCM.
-            - Năm 2010, công ty góp vốn 10 triệu USD vào công ty Miraka Limited.
-            - Năm 2012, công ty thành lập nhà máy sữa Đà Nẵng.
-            - Năm 2013, công ty khánh thành Nhà máy sữa bột Việt Nam, Nhà máy sữa Việt Nam; Công ty TNHH Bò sữa Thống Nhất Thanh Hóa trở thành một công ty con của Vinamilk với 96.11% VĐL do Vinamilk nắm giữ; Mua 70% cổ phần Driftwood Dairy Holdings Corporation tại bang California, Mỹ.
-            - Ngày 27/06/2007, công ty trả cổ tức bằng Tiền, tỷ lệ 19%.
-            - Ngày 31/01/2007, công ty trả cổ tức bằng Tiền, tỷ lệ 10%.
+        Tình hình cổ phiếu VNM từ ngày mùng 4 tháng 10 trở về trước có những thông tin chính sau đây.....
+            
     (VNM, 10/04/2023, 09/28/2023)
 
 Ví dụ 2:
     Câu hỏi: 
         Cổ phiếu Vingroup từ ngày 25 tháng 9 đến ngày mùng 4 tháng 10?
     Câu trả lời: 
-        Tình hình cổ phiếu Vingroup từ ngày 25 tháng 9 đến ngày mùng 4 tháng 10 có những thông tin chính sau đây:
-            - Cổ phiếu VIC của tập đoàn Vingroup giảm sàn trong phiên giao dịch ngày 25/9, về mức giá 46.500 đồng/cp, là mức giá thấp nhất của cổ phiếu này từ tháng 11/2017.
-            - Thị giá của cổ phiếu VIC đã giảm đến 38,5% so với mức đạt đỉnh vào hôm 16/8 với mức giá 75.200 đồng/cp.
-            - Vốn hóa thị trường của Vingroup đã giảm 111.000 tỷ đồng chỉ trong vòng hơn 1 tháng.
-            - Đồ thị giá 3 tháng/6 tháng/12 tháng của cổ phiếu VIC đã được vẽ bằng dữ liệu điều chỉnh.
-    (VIC, 10/04/2023, 09/25/2023)
-"""
+        Tình hình cổ phiếu Vingroup từ ngày 25 tháng 9 đến ngày mùng 4 tháng 10 có những thông tin chính sau đây......
 
+    (VIC, 10/04/2023, 09/25/2023)
+
+Ví dụ 3:
+    Câu hỏi:
+        Diện tích đất nước Việt Nam là bao nhiêu?
+    Câu trả lời:
+        Diện tích Việt Nam là......
+
+"""
 QA_CHAIN_PROMPT_2 = ChatPromptTemplate.from_template(template_2)
