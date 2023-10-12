b6b4c7993997a6015e7b36f51f2fd86b44736ece
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2023-10-12 19:15:08 +0700

    remove unneccesary files and folders

--- backend_ref/repositories/__init__.py
+++
@@ -1,2 +0,0 @@
-# history là table lưu trữ
-# history_repository là object để thực hiện CRUD (hành động)
--- backend_ref/utils/date_utils.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-
-def get_time_format() -> str:
-    now = datetime.datetime.utcnow()
-    # y-m-d
-    out = f'{now.year}-{now.month}-{now.day}'
-    return out
